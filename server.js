5b86f74f59dd501f4c5e60982ee60f3dbb864d82
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2019-12-11 19:26:36 +0200

    get rajapinta lisatty

--- server.js
+++ server.js
@@ -6,3 +6,21 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("kuuntelen porttia " + port));
 
 app.use(express.static("public"));
+
+app.use(express.json({limit: '1mb'}));
+
+
+const pisteet = [
+    {
+        "pelaaja": "Suvi",
+        "pisteet": "86"
+    },
+    {
+      "pelaaja": "Mary",
+      "pisteet": "25"
+    }
+]
+
+app.get('/api/pisteet', function (request, response) {
+  response.send(pisteet);
+})
